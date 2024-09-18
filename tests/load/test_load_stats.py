dba91a459e9a3462cae4f09e7f6650102191a30a
Krzysztof Bober <krzysztof.bober@inexto.com>
2024-09-18 16:35:02 +0200

    Enable post requests on the report

--- tests/load/test_load_stats.py
+++ tests/load/test_load_stats.py
@@ -8,7 +8,7 @@ from locust import between
 from locust.env import Environment
 from locust.stats import stats_history, stats_printer, StatsCSVFileWriter
 
-from config.consts import TEST_SYMBOL, MAX_K_VALUE
+from config.consts import TEST_SYMBOL, MAX_K_VALUE, MAX_TRADE_POINTS_COUNT
 from helpers.generators import generate_values
 from main import LOG
 from sender import Sender
@@ -35,7 +35,7 @@ class UsersTest(locust.TaskSet):
     user interactions with the API endpoints, such as getting statistics and adding batches.
     """
 
-    @locust.task
+    @locust.task(10)
     def get_stats(self):
         """
         Send GET requests to the /stats endpoint for different values of 'k'.
@@ -43,6 +43,15 @@ class UsersTest(locust.TaskSet):
         """
         self.client.get(f"/stats?symbol={TEST_SYMBOL}&k={MAX_K_VALUE}")
         time.sleep(1)  # Pause to simulate realistic user wait times between requests
+
+    @locust.task(1)
+    def add_batch(self):
+        """
+        Add more data to be sure it doesn't have much impact on performance (avg response time should be pretty stable).
+        :return:
+        """
+        values = generate_values(MAX_TRADE_POINTS_COUNT)
+        self.client.post("/add_batch", json={"symbol": TEST_SYMBOL, "values": list(values)})
 
 
 class LoadTest(locust.HttpUser):
@@ -52,6 +61,34 @@ class LoadTest(locust.HttpUser):
 
 
 def test_locust_load_stats():
+    """
+    This test uses Locust to perform a load test on the /stats endpoint. It simulates multiple
+    users making requests to the API and tracks how the response time changes as the number of
+    requests and data points increases.
+
+    Key points of the test:
+    - The test simulates 100 concurrent users interacting with the API, sending requests to the
+      /stats endpoint and adding batches of data.
+    - The test dynamically spawns users at a rate of 10 new users per second.
+    - The simulated user behavior includes sending GET requests to retrieve statistics for a symbol
+      and sending POST requests to add new batches of trading data.
+    - Each user also add some new data to the system (1 POST request to /add_batch after  10 GET /stats requests)
+    - The test collects performance data (such as response times and request rates) and stores it
+      in CSV format. This data is periodically saved to disk for analysis.
+    - The WebUI is started to monitor real-time statistics during the test.
+    - After 30 minutes of testing, the load test automatically stops.
+
+    Test workflow:
+    1. Setup the Locust environment, including starting the runner and WebUI.
+    2. Simulate user interactions with the API, where the majority of interactions (weight 10)
+       involve sending requests to the /stats and /add_batch endpoints.
+    3. Collect performance metrics and save them to CSV files.
+    4. After 30 minutes, stop the load test and finalize the results.
+
+    This test helps evaluate the scalability of the /stats endpoint and observe how the
+    system performs under high load, with special attention to response time as the
+    number of data points and concurrent users increases.
+    """
     # setup Environment and Runner
     env = Environment(user_classes=[LoadTest], events=locust.events)
     runner = env.create_local_runner()
@@ -61,7 +98,7 @@ def test_locust_load_stats():
         environment=env,
         base_filepath=stats_path,
         full_history=True,
-        percentiles_to_report=[90.0, 95.0]
+        percentiles_to_report=[10.0, 90.0]
     )
     gevent.spawn(csv_writer)
 
@@ -74,17 +111,17 @@ def test_locust_load_stats():
     # start a greenlet that periodically outputs the current stats
     gevent.spawn(stats_printer(env.stats))
 
-    # start a greenlet that save current stats to history
+    # start a greenlet that saves current stats to history
     gevent.spawn(stats_history, env.runner)
 
     # start the test
     runner.start(user_count=100, spawn_rate=10)
 
-    # in 30 seconds stop the runner
-    gevent.spawn_later(300, runner.quit)
+    # in 30 minutes stop the runner
+    gevent.spawn_later(1800, runner.quit)  # 1800 seconds = 30 minutes
 
     # wait for the greenlets
     runner.greenlet.join()
 
-    # stop the web server for good measures
+    # stop the web server for good measure
     # web_ui.stop()
